45e9cc35f649f9896c873ffa92606b47062a8709
ldpina <ldpina@ucsd.edu>
2022-05-01 12:57:59 -0700

    Update skillDemo3Test.java

--- skillDemo3Test.java
+++ skillDemo3Test.java
@@ -1,9 +1,10 @@
-import static org.junit.Assert.*;
 import org.junit.*;
+import static org.junit.Assert.*;
+
 
 public class skillDemo3Test
 {
-    @test
+    @Test
     public void tester()
     {
         assertEquals(1, skillDemo3.evenOrOdd(2));
